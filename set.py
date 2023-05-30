7a0a2cf186855999c38fd471bbc11db3d653cadd
Justin Jolly <83817846+Justinjolly@users.noreply.github.com>
2023-05-30 09:01:36 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,10 @@
+a = {1, 2, 3}
+b = {3, 4, 5}
+print("SET OPERATIONS")
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+print(a.issubset(b))
+print(a.issuperset(b))
+print(a == b)
+print(a != b)
